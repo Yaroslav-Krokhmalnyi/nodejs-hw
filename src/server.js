2f23ae86c935ad29b5eb35c76c9a5b5b55bcfa89
Yaroslav Krokhmalnyi <krokhmalniy.code@gmail.com>
2026-02-18 22:05:35 +0200

    Create validation logic

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
@@ -18,6 +19,8 @@ app.use(cors());
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
+
+app.use(errors());
 
 app.use(errorHandler);
 
---
+++ src/validations/notesValidation.js
@@ -0,0 +1,111 @@
+import { Joi, Segments } from 'celebrate';
+import { isValidObjectId } from 'mongoose';
+
+export const getAllNotesSchema = {
+  [Segments.QUERY]: Joi.object({
+    page: Joi.number().integer().min(1).default(1).messages({
+      'number.base': 'Page must be a number',
+      'number.min': 'Page must be at least {#limit}',
+    }),
+    perPage: Joi.number().integer().min(5).max(20).default(10).messages({
+      'number.base': 'perPage must be a integer number',
+      'number.min': 'perPage must be at least {#limit}',
+      'number.max': 'perPage must be at most {#limit}',
+    }),
+    tag: Joi.string()
+      .valid(
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      )
+      .messages({
+        'any.only':
+          'Tag must be one of: work, personal, meeting, shopping, ideas, travel, finance, health, important, todo',
+      }),
+    search: Joi.string().default('').messages({
+      'string.base': 'Search must be a string',
+    }),
+  }),
+};
+
+const objectIdValidator = (value, helpers) => {
+  return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
+};
+
+export const noteIdSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(objectIdValidator).required(),
+  }),
+};
+
+export const createNoteSchema = {
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).required().messages({
+      'string.base': 'Title must be a string',
+      'string.min': 'Title must contain at least 1 character',
+      'any.required': 'Title is required',
+    }),
+    content: Joi.string().allow('').messages({
+      'string.base': 'Content must be a string',
+    }),
+    tag: Joi.string()
+      .valid(
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      )
+      .messages({
+        'any.only':
+          'Tag must be one of: work, personal, meeting, shopping, ideas, travel, finance, health, important, todo',
+      }),
+  }).min(1),
+};
+
+export const updateNoteSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(objectIdValidator).required(),
+  }),
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).messages({
+      'string.base': 'Search must be a string',
+    }),
+    content: Joi.string().messages({
+      'string.base': 'Content must be a string',
+    }),
+    tag: Joi.string()
+      .valid(
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      )
+      .messages({
+        'any.only':
+          'Tag must be one of: work, personal, meeting, shopping, ideas, travel, finance, health, important, todo',
+      }),
+  })
+    .min(1)
+    .messages({
+      'object.min': 'Must be at least 1 key',
+    }),
+};
