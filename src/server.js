5c5912f38e49924ae0cea4c78f35d09ef9887681
Yaroslav Krokhmalnyi <krokhmalniy.code@gmail.com>
2026-03-15 22:20:30 +0200

    Create avatar logic

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import cookieParser from 'cookie-parser';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -21,6 +22,7 @@ app.use(cookieParser());
 
 app.use(authRoutes);
 app.use(notesRoutes);
+app.use(userRoutes);
 
 app.use(notFoundHandler);
 
